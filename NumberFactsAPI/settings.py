df0727aa42cfad0dbd8e9d9b8eeaa6f81d89902b
Rodney Njau <marknjoropge@gmail.com>
2025-02-07 18:29:35 +0300

    Add render to allowed hosts

--- NumberFactsAPI/settings.py
+++ NumberFactsAPI/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-gfdua1*dn3a&g@s5u(iss#&so!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  "number-facts-api-r8lg.onrender.com"
+]
 
 REST_FRAMEWORK ={
   "DEFAULT_RENDERER_CLASSES" : ["rest_framework.renderers.JSONRenderer"]
